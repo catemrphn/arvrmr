121259577ef3dbb24158ba8f62ffc131fe4671a0
Катя Халикова <cate@MacBookPro-Cate.local>
2019-04-20 16:26:02 +0300

    edit first block

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    let margin=80;
+    $("a[href*=#]").on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top-margin
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
