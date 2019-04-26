a539e3303efdef92e1440d837479d72520ff31ad
Катя Халикова <cate@MacBookPro-Cate.local>
2019-04-26 21:49:34 +0300

    finally

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//плавный переход по кнопкам меню
+
 $(document).ready(function () {
     let margin=70;
     $("a[href*=#]").on("click", function (e) {
@@ -10,12 +12,91 @@ $(document).ready(function () {
     });
 });
 
+// гамбургер меню
 
-// $("#form-row").vide('video.mp4', {
-//     volume: 1, // звук
-//     playbackRate: 1, // скорость воспроизведения
-//     muted: true, // отключен ли звук
-//     loop: true, // закцикливание
-//     autoplay: true, // автовоспроизведение
-//     position: "50% 50%" // выравнивание
+(function($){
+    $(function() {
+        $('.menu__icon').on('click', function() {
+            $(this).closest('.menu_xs')
+                .toggleClass('menu_state_open');
+        });
+
+        $('.menu__links-item').on('click', function() {
+            // do something
+
+            $(this).closest('.menu_xs')
+                .removeClass('menu_state_open');
+        });
+    });
+})(jQuery);
+
+
+//
+//
+// $('#logo_AR').mousemove(function(e){
+//     var amountMovedX = e.pageX * 1/100;
+//     var amountMovedY = e.pageY * 1/100;
+//     $(this).css('top', amountMovedX + 'px');
+//     $(this).css('left', amountMovedY + 'px');
+//
 // });
+//
+//
+//
+
+// анимация лого AR
+
+(function(){
+    let body = document.body,
+        image = document.getElementById('logo_AR'),
+        startX = 15,
+        startY = 20,
+        w = document.documentElement.offsetWidth,
+        h = document.documentElement.offsetHeight;
+
+    body.addEventListener('mousemove', function(evt){
+        var posX = Math.round(evt.clientX / w * startX);
+        var posY = Math.round(evt.clientY / h * startY);
+        image.style.top = posY + 'px';
+        image.style.left = posX + 'px';
+
+    })
+})();
+
+// анимация лого VR
+
+(function(){
+    let body = document.body,
+        image = document.getElementById('logo_VR'),
+        startX = 10,
+        startY = 30,
+        w = document.documentElement.offsetWidth,
+        h = document.documentElement.offsetHeight;
+
+    body.addEventListener('mousemove', function(evt){
+        var posX = Math.round(evt.clientX / w * startX);
+        var posY = Math.round(evt.clientY / h * startY);
+        image.style.top = posY + 'px';
+        image.style.left = posX + 'px';
+
+    })
+})();
+
+// анимация лого MR
+
+(function(){
+    let body = document.body,
+        image = document.getElementById('logo_MR'),
+        startX = 20,
+        startY = 40,
+        w = document.documentElement.offsetWidth,
+        h = document.documentElement.offsetHeight;
+
+    body.addEventListener('mousemove', function(evt){
+        var posX = Math.round(evt.clientX / w * startX);
+        var posY = Math.round(evt.clientY / h * startY);
+        image.style.top = posY + 'px';
+        image.style.left = posX + 'px';
+
+    })
+})();
